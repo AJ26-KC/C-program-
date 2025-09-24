3367814724a3f2755a070579f40f3c9ea74b9f35
AJ26-KC <Kongaalice40@gmail.com>
2025-09-24 11:23:46 +0300

    Add files via upload

---
+++ c program for height, ID no and bank balance.c
@@ -0,0 +1,20 @@
+/*
+Name:konga Alice Jematian 
+Reg:PA106/G/28717/25
+Description:program to prompt the user
+*/
+#include<stdio.h>
+
+int main() {
+    float height;
+    int id_num;
+    float bank_balance;
+    printf("Enter your height \n");
+    scanf("%f",&height);
+    printf("Enter your ID number \n");
+    scanf("%d",&id_num);
+    printf("Enter your bank balance \n");
+    scanf("%f",&bank_balance);
+    
+    return 0;
+    }
