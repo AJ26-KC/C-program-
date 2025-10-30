4e1e0451a5b1d8e0226c5b0e89a9980efca1fe9f
PA106/G/28717/25 <Kongaalice40@gmail.com>
2025-10-30 10:31:04 +0300

    Add files via upload

---
+++ 3D_room_occupancy.c
@@ -0,0 +1,43 @@
+/*
+Name:Konga Jematian Alice 
+Reg no:PA106/G/28717/25
+Description:c program for 3D array on room occupancy 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (){
+     int chain [3] [5] [10];
+     int branch,floor ,room;
+     int totalOccupied =0;
+     srand (time (0));
+     
+     for(branch=0; branch <3;branch++){
+        for(floor=0;floor<5;floor++){
+           for(room=0;room<10;room++){
+               chain[branch][floor][room]=rand()%2;
+           }
+        } 
+     }   
+     
+     for (branch =0;branch <3; branch++){
+         for(floor=0;floor<5;floor++){
+            for(room=0;room<10;room++){
+               if (chain[branch][floor][room]==1)
+                   totalOccupied++;
+               }    
+            }        
+         }  
+         
+         printf("Total occupied rooms across all branches=%d\n",totalOccupied);
+         
+              return 0;  
+         }                       
+   
+
+             
+                            
+
+     
